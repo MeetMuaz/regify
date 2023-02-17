abf1cce616935777a3628bcf885ca19ea8c4b561
meetmuaz <muazbalogun97@gmail.com>
2023-02-17 20:44:02 +0100

    🎲final commit

--- app.js
+++ app.js
@@ -2,49 +2,97 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const Register = require("./models/register");
 
-// ///////// setup connection ////////
-// const { MONGO_URI } = process.env;
-// mongoose.set("strictQuery", false);
-// mongoose.connect(MONGO_URI)
-// .then(()=> {
-//     console.info("REGIFY => connected!");
-// })
-// .catch((err)=>{
-//     console.info("REGIFY => not connected!" + err); 
-// });
-
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
-const { MONGO_URI } = process.env;
-const db = mongoose.connect(MONGO_URI);
-
+// const { MONGO_URI } = process.env;
+const db = mongoose.connect("mongodb://127.0.0.1:27017/regify");
 
 
 ///////// add register ////////
 const addRegister = (register) => {
     Register.create(register).then(register => {
-        console.log('New Customer Added');
-        // mongoose.connection.close();
-        db.close();
+        console.info('100% / 100%');
+        console.info('New Register Added');
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.info(err);
+        console.info('0% / 100%');
+        mongoose.connection.close();
+    })
+}
+
+///////// find all register ////////
+const findAllRegister = () => {
+    Register.find()
+    .then(register =>{
+        console.info(register);
+        console.info(`${register.length} => matches`);
+        console.info('100% / 100%');
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.info(err);
+        console.info('0% / 100%');
+        mongoose.connection.close();
+    }
+    );
+}
+
+
+///////// find register with parameter ////////
+const findOneRegister = (name) => {
+    const search = new RegExp(name,'i');
+    Register.find({$or: [{ firstname: search}, { lastname: search }]})
+    .then(register =>{
+        console.info(register);
+        console.info(`${register.length} => matches`);
+        console.info('100% / 100%');
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.info(err);
+        console.info('0% / 100%');
+        mongoose.connection.close();
     })
 }
 
 
-///////// find register ////////
-const findRegister = (name) => {
-    const search = new RegExp(name,'i');
-    Register.find({$or: [{ fistname: search}, { lastname: search }]})
-    .then(register =>{
-        console.info(customer);
-        console.info(`${register.length} => matches`);
-        // mongoose.connection.close();
-        db.close();
-    });
+///////// update register ////////
+const updateRegister = (_id, register) => {
+    Register.updateOne({_id}, {$set: register})
+    .then(register => {
+        console.info("Existing Register Updated");
+        console.info('100% / 100%');
+        mongoose.connection.close()
+    })
+    .catch((err) => {
+        console.info(err);
+        console.info('0% / 100%');
+        mongoose.connection.close();
+    })
 }
 
+///////// remove register ////////
+const removeRegister = (_id) => {
+    Register.deleteOne({ _id})
+    .then(register => {
+        console.info("Existing Register Removed");
+        mongoose.connection.close();
+        console.info('100% / 100%');
+    })
+    .catch((err) => {
+        console.info(err);
+        console.info('0% / 100%');
+        mongoose.connection.close();
+    })
+}
 
 module.exports = {
     addRegister,
-    findRegister
+    findAllRegister,
+    findOneRegister,
+    updateRegister,
+    removeRegister,
 }
 
--- commands.js
+++ commands.js
@@ -1,29 +1,118 @@
+#! /usr/bin/env node
 const { Command, program } = require('commander');
+const { prompt } = require('inquirer');
+// npm i --save-dev @types/inquirer
+
+//Register Questions
+const questions = [
+    {
+        type: 'input',
+        name: 'firstname',
+        messsage: 'Register Firstname:'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        messsage: 'Register Lastname:'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        messsage: 'Register Phone:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        messsage: 'Register Email:'
+    }
+];
+
 const { argv } = require('process');
 
 const {
     addRegister,
-    findRegister
+    findAllRegister,
+    findOneRegister,
+    updateRegister,
+    removeRegister
 } = require('./app');
 
 program
   .version('0.0.0')
   .description('cli registration system');
 
+
+// CREATE COMMAND
 program
-    .command('add <firstname> <lastname> <phone> <email>')
-    .alias('r-a')
-    .description('Add a customer')
-    .action((firstname, lastname, phone, email) => {
-        addRegister({firstname, lastname, phone, email});
+    .command('add')
+    .alias('a')
+    .description('add a register')
+    .action(() => {
+        prompt(questions).then(answers => addRegister(answers))
     });
 
+// READ ALL COMMAND
+program 
+    .command('list')
+    .alias('l')
+    .description('list all register')
+    .action(() => {
+        findAllRegister();
+    });
+
+// READ ONE COMMAND
 program 
     .command('find <name>')
-    .alias('r-f')
-    .description('find a customer')
+    .alias('f')
+    .description('find a register')
     .action(name => {
-        findRegister(name);
+        findOneRegister(name);
     });
 
+// UPDATE COMMAND
+program 
+    .command('update <_id>')
+    .alias('u')
+    .description('update a register')
+    .action(_id => {
+        prompt(questions).then(answers => updateRegister(_id, answers));
+    });
+
+// DELETE COMMAND
+program 
+    .command('delete <_id>')
+    .alias('d')
+    .description('find a register')
+    .action( _id => {
+            removeRegister(_id);
+    });
+
+
 program.parse(process.argv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// program
+//     .command('add <firstname> <lastname> <phone> <email>')
+//     .alias('a')
+//     .description('add a register')
+//     .action((firstname, lastname, phone, email) => {
+//        addRegister(firstname, lastname, phone, email)
+//     });
